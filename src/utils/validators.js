be0f763ba0f87dcff8610a086939d5edb6894cb8
jesusherrera94 <jesusherrera1594@gmail.com>
2024-03-22 22:43:51 -0600

    create firebase adapter

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -6,3 +6,24 @@ export const isEmailValid = (email) => {
 export const isPasswordValid = (password) => {
     return password.length >= 6;
 };
+
+export const validateEnvs = (config) => {
+    // add required envs to validate for other developers
+    const requiredKeys = [
+      'FIREBASE_API_KEY',
+      'FIREBASE_AUTH_DOMAIN',
+      'FIREBASE_PROJECT_ID',
+      'FIREBASE_STORAGE_BUCKET',
+      'FIREBASE_MESSAGE_SENDER_ID',
+      'FIREBASE_APP_ID',
+    ];
+  
+    // Check if all required keys are present and have non-empty values
+    for (const key of requiredKeys) {
+      if (!(key in config) || !config[key]) {
+        console.error(`Variable ${key} is not defined`);
+      }
+    }
+  
+    return true;
+  };
